ba7eb394df592eb09a0daae7480dcbb6b895d6eb
KarenNR <74948680+KarenNR@users.noreply.github.com>
2023-03-07 00:14:23 -0600

    Code formatting

--- Controllers/QRController.cs
+++ Controllers/QRController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
 using System.Data;
 using Newtonsoft.Json;
@@ -16,10 +15,10 @@ public class QRController : ControllerBase {
         _configuration = configuration;
     }
 
-    // Function that returns the corresponding attendance code, based on the current time and the class hour
+    // --- FUNCTION THAT RETURNS THE CORRESPONDING ATTENDANCE CODE, BASED ON THE CURRENT TIME AND THE CLASS HOUR ---
     private int GetAttendanceCode(int currentClass, TimeSpan classHour, TimeSpan currentTime, DateTime date, bool start) {
         TimeSpan tenMinutes = TimeSpan.FromMinutes(10);
-        // Start of class
+        // START OF CLASS
         if (start) {
             // Class is on time
             if (classHour >= currentTime.Subtract(tenMinutes) && classHour <= currentTime.Add(tenMinutes))
@@ -28,24 +27,25 @@ public class QRController : ControllerBase {
             else
                 return 1;
         }
-        // End of class
+        // END OF CLASS
         else {
             // Get current code of class
             SqlConnection con = new SqlConnection(_configuration?.GetConnectionString("UDEMAppCon")?.ToString());
             SqlDataAdapter da = new SqlDataAdapter("SELECT idCódigo FROM Asistencia WHERE idHorario=" + currentClass + "AND Fecha='" + date.ToString("yyyyMMdd") + "'", con);
             DataTable dt = new DataTable();
             da.Fill(dt);
-            int currentCode = 1; // No se tomó asistencia al inicio de la clase
-            // Se tomó asistencia al inicio de la clase
+
+            int currentCode = -1;
+            // Current code (registered at the beginning of the class)
             if (dt.Rows.Count > 0) {
                 currentCode = (int)dt.Rows[0]["idCódigo"];
             }
             // Class ended soon
             if (currentTime < classHour.Subtract(tenMinutes)) {
-                // Class was not late
+                // Class was not late (beginning)
                 if (currentCode == 0)
                     return 2;
-                // Class was also late
+                // Class was also late (beginning)
                 else
                     return 3;
             }
@@ -55,7 +55,8 @@ public class QRController : ControllerBase {
         }
     }
 
-    // Function that returns the description associated with an attendance code
+
+    // --- FUNCTION THAT RETURNS THE DESCRIPTION ASSOCIATED WITH AN ATTENDANCE CODE ---
     private string GetCodeMessage(int code) {
         SqlConnection con = new SqlConnection(_configuration?.GetConnectionString("UDEMAppCon")?.ToString());
         SqlDataAdapter da = new SqlDataAdapter("SELECT Descripción FROM Códigos WHERE idCódigo=" + code, con);
@@ -64,7 +65,8 @@ public class QRController : ControllerBase {
         return (string)dt.Rows[0]["Descripción"];
     }
 
-    // Function that registers an attendance on a certain date
+
+    // --- FUNCTION THAT REGISTERS AN ATTENDANCE IN THE DATABASE AND RETURNS THE ATTENDANCE CODE ---
     private int SearchAttendance(int nomina, TimeSpan time, DateTime date, string day, bool start) {
         // Get the class that the professor is currently on
         SqlConnection con = new SqlConnection(_configuration?.GetConnectionString("UDEMAppCon")?.ToString());
@@ -75,149 +77,137 @@ public class QRController : ControllerBase {
         DataTable dt = new DataTable();
         da.Fill(dt);
 
-        // Get data of current class
-        int currentClass = (int)dt.Rows[0]["idHorario"];
-        TimeSpan startHour = (TimeSpan)dt.Rows[0]["Hora_Inicio"];
-        TimeSpan endHour = (TimeSpan)dt.Rows[0]["Hora_Final"];
+        // The professor is currently on that class
+        if (dt.Rows.Count > 0) {
+            // Get data of current class
+            int currentClass = (int)dt.Rows[0]["idHorario"];
+            TimeSpan startHour = (TimeSpan)dt.Rows[0]["Hora_Inicio"];
+            TimeSpan endHour = (TimeSpan)dt.Rows[0]["Hora_Final"];
 
-        // Check if there's an attendance field for this class and date
-        con = new SqlConnection(_configuration?.GetConnectionString("UDEMAppCon")?.ToString());
-        da = new SqlDataAdapter(@"
-        SELECT COUNT(*) AS Conteo FROM Asistencia JOIN Horarios ON Asistencia.idHorario=Horarios.idHorario
-        WHERE Asistencia.idHorario=" + currentClass + "AND Fecha='" + date.ToString("yyyyMMdd") + "'", con);
-        dt = new DataTable();
-        da.Fill(dt);
+            // Check if there's an attendance field for this class and date
+            con = new SqlConnection(_configuration?.GetConnectionString("UDEMAppCon")?.ToString());
+            da = new SqlDataAdapter(@"
+            SELECT COUNT(*) AS Conteo FROM Asistencia JOIN Horarios ON Asistencia.idHorario=Horarios.idHorario
+            WHERE Asistencia.idHorario=" + currentClass + "AND Fecha='" + date.ToString("yyyyMMdd") + "'", con);
+            dt = new DataTable();
+            da.Fill(dt);
 
-        // Aún no hay asistencia, independientemente de si inició o terminó la clase
-        if ((int)dt.Rows[0]["Conteo"] == 0) {
-            int code;
-            if (start)
-                code = GetAttendanceCode(currentClass, startHour, time, date, true);
-            else
-                return -2;
-            // Check corresponding code based on time and register it in the database
-            con = new SqlConnection(_configuration?.GetConnectionString("UDEMAppCon")?.ToString());
-            SqlCommand cmd = new SqlCommand("INSERT INTO Asistencia VALUES (" + currentClass + ", '" + date.ToString("yyyyMMdd") +"', " + code + ")", con);
-            con.Open();
-            cmd.ExecuteNonQuery();
-            con.Close();
-            return code;
+            // There is no attendance for that day
+            if ((int)dt.Rows[0]["Conteo"] == 0) {
+                int code;
+                // Beginning of the class
+                if (start)
+                    code = GetAttendanceCode(currentClass, startHour, time, date, true);
+                // End of the class (return an error saying that entrance attendance must be registered first)
+                else
+                    return -2;
+                // Register attendance in database
+                con = new SqlConnection(_configuration?.GetConnectionString("UDEMAppCon")?.ToString());
+                SqlCommand cmd = new SqlCommand("INSERT INTO Asistencia VALUES (" + currentClass + ", '" + date.ToString("yyyyMMdd") +"', " + code + ")", con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                return code;
+            }
+            // End of the class - Update attendance code
+            else if(!start) {
+                int code = GetAttendanceCode(currentClass, endHour, time, date, false);
+                con = new SqlConnection(_configuration?.GetConnectionString("UDEMAppCon")?.ToString());
+                SqlCommand cmd = new SqlCommand("UPDATE Asistencia SET idCódigo=" + code + " WHERE idHorario=" + currentClass, con);                
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                return code;
+            }
+            // The initial attendance has already been taken
+            else {
+                return -1;
+            }
         }
-        // Se está terminando la clase
-        else if(!start) {
-            int code = GetAttendanceCode(currentClass, endHour, time, date, false);
-            con = new SqlConnection(_configuration?.GetConnectionString("UDEMAppCon")?.ToString());
-            SqlCommand cmd = new SqlCommand("UPDATE Asistencia SET idCódigo=" + code + " WHERE idHorario=" + currentClass, con);                
-            con.Open();
-            cmd.ExecuteNonQuery();
-            con.Close();
-            return code;
-        }
+        // The professor isn't currently in a class
         else {
-            return -1;
+            return -3;
         }
     }
 
+    // --- FUNCTION THAT REGISTERS THE ATTENDANCE FOR THE CURRENT DAY ---
     private int RegisterAttendance(Attendance attendance, bool start) {
-        // Get current date
-        DateTime now = DateTime.Now;
-
-        // Get current day of week
-        int dayOfWeek = (int)now.DayOfWeek;
-
-        // Get current time
-        TimeSpan time = now.TimeOfDay;
-
-        // Get current day (date)
-        DateTime date = now.Date;
+        DateTime now = DateTime.Now; // Get current date
+        int dayOfWeek = (int)now.DayOfWeek; // Get current day of week
+        TimeSpan time = now.TimeOfDay; // Get current time
+        DateTime date = now.Date; // Get current day (date)
 
         int code;
 
         switch(dayOfWeek) {
             case 0: // Sunday
-                code = SearchAttendance(attendance?.nomina ?? 0, time, date, "S1", start);
-                break;
+                code = SearchAttendance(attendance?.nomina ?? 0, time, date, "S1", start); break;
             case 1: // Monday
-                code = SearchAttendance(attendance?.nomina ?? 0, time, date, "M", start);
-                break;
+                code = SearchAttendance(attendance?.nomina ?? 0, time, date, "M", start); break;
             case 2: // Tuesday
-                code = SearchAttendance(attendance?.nomina ?? 0, time, date, "T", start);
-                break;
+                code = SearchAttendance(attendance?.nomina ?? 0, time, date, "T", start); break;
             case 3: // Wednesday
-                code = SearchAttendance(attendance?.nomina ?? 0, time, date, "W", start);
-                break;
+                code = SearchAttendance(attendance?.nomina ?? 0, time, date, "W", start); break;
             case 4: // Thursday
-                code = SearchAttendance(attendance?.nomina ?? 0, time, date, "R", start);
-                break;
+                code = SearchAttendance(attendance?.nomina ?? 0, time, date, "R", start); break;
             case 5: // Friday
-                code = SearchAttendance(attendance?.nomina ?? 0, time, date, "F", start);
-                break;
+                code = SearchAttendance(attendance?.nomina ?? 0, time, date, "F", start); break;
             case 6: // Saturday
-                code = SearchAttendance(attendance?.nomina ?? 0, time, date, "S", start);
-                break;
+                code = SearchAttendance(attendance?.nomina ?? 0, time, date, "S", start); break;
             default:
-                code = -1;
-                break;
+                code = -1; break;
         }
 
         return code;
     }
 
+
+    // --- API ROUTE: REGISTER ENTRANCE ATTENDANCE ---
     [HttpPost]
     [Route("RegisterEntrance")]
     public IActionResult RegisterEntrance(Attendance attendance) {
         int code = RegisterAttendance(attendance, true);
-        return Ok(new { code, message = (code == -1 ? "La asistencia inicial ya se ha registrado. Si desea registrar la salida, genere un código de salida." : GetCodeMessage(code)) });
+        return Ok(new { code, message = (code == -1 ? "La asistencia inicial ya se ha registrado. Si desea registrar la salida, genere un código de salida." : code == -3 ? "No hay clases en este momento." : GetCodeMessage(code)) });
     }
 
+
+    // --- API ROUTE: REGISTER DEPARTURE ATTENDANCE ---
     [HttpPost]
     [Route("RegisterDeparture")]
     public IActionResult RegisterDeparture(Attendance attendance) {
         int code = RegisterAttendance(attendance, false);
-        return Ok(new { code, message = (code == -2 ? "No se ha registrado la asistencia inicial." : GetCodeMessage(code)) });
+        return Ok(new { code, message = (code == -2 ? "No se ha registrado la asistencia inicial." : code == -3 ? "No hay clases en este momento." : GetCodeMessage(code)) });
     }
 
+
+    // --- API ROUTE: GET INFORMATION OF CURRENT COURSE ---
     [HttpGet]
     [Route("GetCourseData/{nomina}")]
     public string GetCourseData(int nomina) {
-        // Get current date
-        DateTime now = DateTime.Now;
-
-        // Get current day of week
-        int dayOfWeek = (int)now.DayOfWeek;
-
-        // Get current time
-        TimeSpan time = now.TimeOfDay;
-
-        // Get current day (date)
-        DateTime date = now.Date;
+        DateTime now = DateTime.Now; // Get current date
+        int dayOfWeek = (int)now.DayOfWeek; // Get current day of week
+        TimeSpan time = now.TimeOfDay; // Get current time
+        DateTime date = now.Date; // Get current day (date)
 
         string day = "";
         switch(dayOfWeek) {
             case 0: // Sunday
-                day = "S1";
-                break;
+                day = "S1"; break;
             case 1: // Monday
-                day = "M";
-                break;
+                day = "M"; break;
             case 2: // Tuesday
-                day = "T";
-                break;
+                day = "T"; break;
             case 3: // Wednesday
-                day = "W";
-                break;
+                day = "W"; break;
             case 4: // Thursday
-                day = "R";
-                break;
+                day = "R"; break;
             case 5: // Friday
-                day = "F";
-                break;
+                day = "F"; break;
             case 6: // Saturday
-                day = "S";
-                break;
+                day = "S"; break;
         }
 
-        // Get the class that the professor is currently on
+        // Get the information of the class that the professor is currently on
         SqlConnection con = new SqlConnection(_configuration?.GetConnectionString("UDEMAppCon")?.ToString());
         SqlDataAdapter da = new SqlDataAdapter(@"
         SELECT Materia, idHorario, Hora_Inicio, Hora_Final FROM Cursos JOIN Horarios ON Cursos.CRN=Horarios.CRN JOIN Materias ON CVE_Materia=CVE
