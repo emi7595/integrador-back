6a6b8cc6da529e21e895dd8b876ac9a2a62dc62b
KarenNR <74948680+KarenNR@users.noreply.github.com>
2023-04-21 00:08:48 -0600

    deleted unnecesary controllers and models

--- Models/Empleado.cs
+++
@@ -1,8 +0,0 @@
-namespace integrador_back.Models;
-
-public class Empleado
-{
-    public string? nomina { get; set; }
-    public string? nombre { get; set; }
-    public string? idRol { get; set; }
-}
